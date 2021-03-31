9813aa96907a92d63b2e99e866173617785eecfc
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-03-31 23:58:27 +0530

    Initialise

--- store/models/product.py
+++ store/models/product.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from .category import Category
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.IntegerField(max_length=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField(max_length=100,default='', blank=True)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='uploads/products/')
